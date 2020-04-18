572a0b2b9363638fbaf27aa27d6fa51df5604590
Kyle Berner <kyleberner14@yahoo.com>
2020-04-17 18:47:03 -0700

    add schema and seeds

---
+++ db/schema.sql
@@ -0,0 +1,13 @@
+-- Drops the todolist if it exists currently --
+DROP DATABASE IF EXISTS burgers_db;
+-- Creates the "todolist" database --
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers(
+  id INTEGER AUTO_INCREMENT NOT NULL,
+  text VARCHAR(255) NOT NULL,
+  devoured BOOLEAN DEFAULT false,
+  PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (text, devoured)
+VALUES 
+("Rodeo Clown", false),
+("Veg in a Hedge", false),
+("The Keto Challenger", false);
